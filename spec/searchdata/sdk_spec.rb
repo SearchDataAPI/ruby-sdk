2ef2fb83914355981b9d05fc9e3bab9051f7635b
Raluca Penciuc <penciuc.raluca@yahoo.com>
2021-09-07 17:58:20 +0300

    bundle gem init

---
+++ spec/searchdata/sdk_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Searchdata::Sdk do
+  it "has a version number" do
+    expect(Searchdata::Sdk::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
